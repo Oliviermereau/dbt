23e772a7b82d2c86327e1231371b7a2ff2c02067
Olivier Mereau <oliviermereau1@gmail.com>
2024-10-13 00:31:56 +0000

    réponse exemple v2

--- models/marts/marketing/customers.sql
+++ models/marts/marketing/customers.sql
@@ -25,7 +25,7 @@ customer_orders as (
 
 ),
 
-lifetime_value as (
+ltv as (
 
     select
         customer_id,
@@ -38,18 +38,18 @@ lifetime_value as (
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
+        customer_id,
+        first_name,
+        last_name,
+        first_order_date,
+        most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
-    left join lifetime_value using (customer_id)
+    left join ltv using (customer_id)
 
 )
 
